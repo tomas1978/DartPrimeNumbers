683cd93ff21b52b78a9a4bfb1578fe472779453a
tomas1978 <tomas.nilsson78@gmail.com>
2021-06-16 08:44:47 +0200

    Tried to implement isPrime boolean function
    Unfortunately, function call does not yet work.

--- primenumbers.dart
+++ primenumbers.dart
@@ -1,12 +1,18 @@
 void main() {
-  var isPrime = true;
-	var testNumber=53;
-  for(var i=2;i<testNumber;i++) {
-    if(testNumber%i==0)
-      isPrime=false;
-  }
-  if(isPrime)
+  bool isPrime = true;
+	int number=53;
+
+  if(isPrime(number))
     print("Prime number");
   else
     print("Not prime");
+}
+
+int isPrime(int testNumber) {
+  bool prime=true;
+  for(var i=2;i<testNumber;i++) {
+    if(testNumber%i==0)
+      prime=true;
+  }
+  return prime;
 }
